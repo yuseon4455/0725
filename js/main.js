a40518a117246a5860d7c846ebf37d2bb61a82a8
yuseon4455 <105402328+yuseon4455@users.noreply.github.com>
2022-07-26 08:51:19 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// main.js
+$(function(){
+
+  // 모바일 토글 메뉴
+  // #toggle click하면 nav가 toggle됨(toggleClass)
+  $('#toggle').click(function(){
+    $('header nav').toggleClass('on');
+  })
+
+  // slide down 효과로 수정해 보세요(CSS에서 해야 될 듯?)
+
+
+
+}); // $
